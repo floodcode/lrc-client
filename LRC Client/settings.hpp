6d6fbb93ce46d5515c4d63feedf68e95f34563a1
Sergiy Varakuta <flood.code@gmail.com>
2016-03-09 13:18:31 +0200

    Added missing header

---
+++ LRC Client/settings.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+
+
+namespace Settings
+{
+	using std::string;
+
+	static string sha256ID = "fe6340be87fd5e43b7f0cac5741e76205dd69a68b2024fda16c696848a720f7a";
+
+	namespace WebSocketSvc
+	{
+		static string host = "ws://localhost:8080/";
+		static size_t connectionDelay = 5;
+	}
+
+	namespace KeyboardSvc
+	{
+		static size_t eventsToProcess = 5;
+		static size_t maxRepeats = 5;
+	}
+}
